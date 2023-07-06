a3e1c47c326e4f1d1c3f82f33a196d164ca589b4
wallacemaxters <wallacemaxters@gmail.com>
2023-07-06 19:32:39 -0300

    fix composer autoload

---
+++ tests/BladePetiteVueServiceProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use WallaceMaxters\BladePetiteVue\BladePetiteVueServiceProvider;
+
+class BladePetiteVueServiceProviderTest extends TestCase
+{
+    public function test()
+    {
+        new BladePetiteVueServiceProvider;
+    }
+}
